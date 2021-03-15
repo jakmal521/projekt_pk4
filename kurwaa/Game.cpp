40cfecc6fbae39620e8a06359105a56d8525b40a
jakmal521 <jakimalecko@gmail.com>
2021-03-15 15:45:23 +0100

    testy

---
+++ kurwaa/Game.cpp
@@ -0,0 +1,48 @@
+
+#include "Game.h"
+//Konstruktor i destruktor
+Game::Game()
+{
+	initWindow();
+}
+Game::~Game()
+{
+	delete this->window;
+}
+//funkcja uruchamiaj¹ca gre
+void Game::play()
+{
+	while (this->window->isOpen())
+	{
+		this->uptade();
+		this->render();
+	}
+}
+
+
+//Funkcje inicjalizacyjne
+
+void Game::initWindow()
+{
+
+	this->window = new RenderWindow(VideoMode(800, 600), "Total War - wersja studencka", Style::Close | Style::Titlebar);
+	this->window->setFramerateLimit(144);
+
+}
+//Funcje do zmiany zawartoœci ekranu
+void Game::uptade()
+{
+	Event event;
+	//zamkniêcie gry przy wciscniesciu esc lub tego krzy¿yka ugóry XD
+	while (this->window->pollEvent(event))
+	{
+		if ((event.type == Event::Closed) || (event.key.code == Keyboard::Escape))
+			this->window->close();
+	}
+}
+
+void Game::render()
+{
+	this->window->clear();
+	this->window->display();
+}
---
+++ kurwaa/Game.h
@@ -0,0 +1,30 @@
+
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <vector>
+using namespace std;
+using namespace sf;
+class Game
+{
+public:
+	//Konstruktor i destruktor
+	Game();
+	~Game();
+
+	void play();
+private:
+	//Okno g³ówne
+	RenderWindow* window;
+
+	//Funckje inicjalizacyjne
+	void initWindow();
+	//void initVariables();
+
+	//Funcje do zmiany zawartoœci ekranu
+	void uptade();
+	void render();
+};
+
